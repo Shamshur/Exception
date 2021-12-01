cdbb222de8e8569a1afb5851d64ca32bf77bcacd
Shams <shamshurtheone@gmail.com>
2021-12-01 15:30:34 +0100

    Initialise first commit

---
+++ exceptions/src/enumerationTwo/EnumSecond.java
@@ -0,0 +1,22 @@
+package enumerationTwo;
+//An enumeration of Apple variety
+enum Apple{
+	Ram, Sita, Geeta, Peeta
+}
+class EnumSecond {
+	public static void main(String[] args) {
+		Apple ap;
+		System.out.println(" Here is all apple constant:");
+		//use values()
+		Apple allapples[] = Apple.values();
+		//for(Apple a: allapples)
+		for(Apple a: Apple.values())
+			System.out.println(a);
+		System.out.println();
+		//use valueof())
+		ap=Apple.valueOf("Geeta");
+		System.out.println("ap contains " + ap);
+	}
+
+
+}
